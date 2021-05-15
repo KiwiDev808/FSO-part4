a8b34f155ac71275fd7aa3434e8c94eba6a2d3ba
KiwiDev808 <lucasbacela@hotmail.com>
2021-05-14 23:54:46 -0300

    Added favoriteBlog function and the test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,6 +5,26 @@ const dummy = (blogs) => {
   return 0
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return prev.likes > current.likes ? prev : current
+  }
+
+  return blogs.length === 0
+    ? { message: 'the array is empty' }
+    : blogs.reduce(reducer, blogs[0])
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
